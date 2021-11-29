eb665157986efd33165f5a19add734ec5c83b690
minjung-mmc <minilove727@hanyang.ac.kr>
2021-11-29 16:14:30 +0900

    2021-11-29

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import torch
 
 class params:
     def __init__(self):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
         # self.device = "cpu"
         self.train_img_path = "../dataset/CityScape/leftImg8bit/train/"
         self.train_seg_label = "../dataset/CityScape/gtFine/train/"
